3f812c98366c3ea72e4c46c4cd3a75b9c1ad6ced
Rayan Tofique <rayantofique@gmail.com>
2019-06-03 09:44:32 -0400

    completed example

---
+++ src/src/main/java/hello/MessageController.java
@@ -0,0 +1,25 @@
+package src.main.java.hello;
+
+import java.util.concurrent.atomic.AtomicLong;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class MessageController {
+
+
+  private static final String template = "Hello, %s!";
+  private final AtomicLong counter = new AtomicLong();
+
+  //The @RequestMapping annotation ensures that HTTP requests to /greeting are mapped to the greeting() method.
+
+  //value is mapped to the parameter name. a default value is also specified
+  @RequestMapping("/message")
+  public Message message(@RequestParam(value="name", defaultValue="World") String name) {
+    return new Message(counter.incrementAndGet(),
+        String.format(template, name));
+  }
+
+}
